603847f3a15acd68d3f63d2b9c754f363eb2fd53
rafa <rafaelsistem87@gmail.com>
2021-05-01 03:48:23 -0700

    react native ELEMENTS icons

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { Icon } from "react-native-elements";
 
 import RestaurantsStack from "./RestaurantsStack";
 import FavoritesStack from "./FavoritesStack";
@@ -20,7 +20,9 @@ export default function Navigation() {
           inactiveTintColor: "#646464",
           activeTintColor: "#00a680",
         }}
-       
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+        })}
       >
         <Tab.Screen
           name="restaurants"
@@ -49,8 +51,32 @@ export default function Navigation() {
         />
       </Tab.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
+function screenOptions(route, color) {
+  let iconName;
 
-
+  switch (route.name) {
+    case "restaurants":
+      iconName = "compass-outline";
+      break;
+    case "favorites":
+      iconName = "heart-outline";
+      break;
+    case "top-restaurants":
+      iconName = "star-outline";
+      break;
+    case "search":
+      iconName = "magnify";
+      break;
+    case "account":
+      iconName = "home-outline";
+      break;
+    default:
+      break;
+  }
+  return (
+    <Icon type="material-community" name={iconName} size={22} color={color} />
+  );
+}
